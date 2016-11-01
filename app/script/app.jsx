32e5ccf6212ce98d37c0cbf2eeecc0cda0def38c
Nick Soper <nicksoper@gmail.com>
2016-11-01 23:06:00 +0200

    Babel transpiler

---
+++ app/script/app.jsx
@@ -0,0 +1,9 @@
+var Test = React.createClass({
+    render: function(){
+        return (
+            <h1>Welcome</h1>
+        )
+    }
+});
+
+ReactDOM.render(<Test />, document.getElementById("container"));
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 var gulp = require("gulp");
 var gls = require("gulp-live-server");
+var babel = require("gulp-babel");
 
-gulp.task("serve", function(){
-    var server = gls.static('/', 3000);
+gulp.task("serve", ['transpile'], function(){
+    var server = gls.static('app', 3000);
     server.start();
+    
+    gulp.watch('app/**/*.jsx', ['transpile']);
 
-    gulp.watch(['**/*.css', '**/*.js', '**/*.html'], function (file) {
+    gulp.watch(['app/**/*.css', 'app/**/*.js', 'app/**/*.html'], function (file) {
       server.notify.apply(server, [file]);
     });    
 })
 
+gulp.task("transpile", function () {
+  return gulp.src("app/script/*.jsx")
+    .pipe(babel())
+    .pipe(gulp.dest("app/script/"));
+});
